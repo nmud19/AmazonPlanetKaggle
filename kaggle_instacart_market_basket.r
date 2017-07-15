8cf6096727c555c5907c666dc396b9517b917b9d
Nikhil M <nmud19@gmail.com>
2017-07-15 19:33:13 +0530

    Create kaggle_instacart_market_basket.r - By KaggleHub

---
+++ kaggle_instacart_market_basket.r
@@ -0,0 +1,34 @@
+
+###########################################################################################################
+#
+# Kaggle Instacart competition
+# Fabien Vavrand, June 2017
+# Simple xgboost starter, score 0.3791 on LB
+# Products selection is based on product by product binary classification, with a global threshold (0.21)
+#
+###########################################################################################################
+
+library(data.table)
+library(dplyr)
+library(tidyr)
+
+
+# Load Data ---------------------------------------------------------------
+path <- "../input"
+
+aisles <- fread(file.path(path, "aisles.csv"))
+departments <- fread(file.path(path, "departments.csv"))
+orderp <- fread(file.path(path, "order_products__prior.csv"))
+ordert <- fread(file.path(path, "order_products__train.csv"))
+orders <- fread(file.path(path, "orders.csv"))
+products <- fread(file.path(path, "products.csv"))
+
+
+# Reshape data ------------------------------------------------------------
+aisles$aisle <- as.factor(aisles$aisle)
+departments$department <- as.factor(departments$department)
+orders$eval_set <- as.factor(orders$eval_set)
+products$product_name <- as.factor(products$product_name)
+
+products <- products %>% 
+  inner_join(aisles) %>% inner_join(departments) %>% 
